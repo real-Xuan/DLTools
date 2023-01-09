481fe1fd3b65510afc5204d490e28393f75ad171
Xuan Zhao <11705197+real-Xuan@users.noreply.github.com>
2023-01-09 18:31:16 +0800

    Update GPRDataGenerator.py
    Added init function.

--- GPRDataGenerator.py
+++ GPRDataGenerator.py
@@ -1,13 +1,18 @@
 import gprMax
 
-class Generator(){
+class Generator:
+    def __init__(self, model, frequency, amplitude, phase, x, y, z):
+        self.model = model
+        self.frequency = frequency
+        self.amplitude = amplitude
+        self.phase = phase
+        self.x = x
+        self.y = y
+        self.z = z
 
-  function configurations(){
-  
-  }
-  
-  function generate(){
-  
-  }
-  
-}
+    def configurations(self):
+      return 0
+
+    def generate(self):
+      return 0
+
