f38b0d30364d7067f4a32afe046925706df1a856
Xuan Zhao <11705197+real-Xuan@users.noreply.github.com>
2023-01-17 00:43:12 +0800

    Added Files from gprMax Project.
    1. Added "/gprMax/gprMax" directory and it files from gprMax project.
    2. "GPRDataGenerator.py" -> "NormalGPRDataBuilder.py".

--- GPRModule/NormalGPRDataBuilder.py
+++ GPRModule/NormalGPRDataBuilder.py
@@ -5,7 +5,9 @@ import glob
 from abc import abstractmethod
 from rich import print
 from GPRDataBuilder import GPRDataBuilder
-class Generator(GPRDataBuilder):
+from gprMax.model_build_run import run_model
+
+class NormalGPRDataBuilder(GPRDataBuilder):
     def __init__(self, model, frequency, amplitude, phase, x, y, z):
         self.model = model
         self.frequency = frequency
@@ -16,13 +18,50 @@ class Generator(GPRDataBuilder):
         self.z = z
         self.batchSize = 512
 
+    # From gprMax Project
+    def run_std_sim(args, inputfile, usernamespace, optparams=None):
+        """
+        Run standard simulation - models are run one after another and each model
+        is parallelised using either OpenMP (CPU) or CUDA (GPU)
 
-    # def InstanceOfBuilder(GPRDataBuilder):
+        Args:
+            args (dict): Namespace with command line arguments
+            inputfile (object): File object for the input file.
+            usernamespace (dict): Namespace that can be accessed by user in any
+                    Python code blocks in input file.
+            optparams (dict): Optional argument. For Taguchi optimisation it
+                    provides the parameters to optimise and their values.
+        """
 
+        # Set range for number of models to run
+        if args.task:
+            # Job array feeds args.n number of single tasks
+            modelstart = args.task
+            modelend = args.task + 1
+        elif args.restart:
+            modelstart = args.restart
+            modelend = modelstart + args.n
+        else:
+            modelstart = 1
+            modelend = modelstart + args.n
+        numbermodelruns = args.n
 
-
-
-
+        tsimstart = timer()
+        for currentmodelrun in range(modelstart, modelend):
+            # If Taguchi optimistaion, add specific value for each parameter to
+            # optimise for each experiment to user accessible namespace
+            if optparams:
+                tmp = {}
+                tmp.update((key, value[currentmodelrun - 1]) for key, value in optparams.items())
+                modelusernamespace = usernamespace.copy()
+                modelusernamespace.update({'optparams': tmp})
+            else:
+                modelusernamespace = usernamespace
+            run_model(args, currentmodelrun, modelend - 1, numbermodelruns, inputfile, modelusernamespace)
+        tsimend = timer()
+        simcompletestr = '\n=== Simulation completed in [HH:MM:SS]: {}'.format(
+            datetime.timedelta(seconds=tsimend - tsimstart))
+        print('{} {}\n'.format(simcompletestr, '=' * (get_terminal_width() - 1 - len(simcompletestr))))
 
     def get_output_data(filename, rxnumber, rxcomponent):
 
@@ -81,7 +120,6 @@ class Generator(GPRDataBuilder):
         f.close()
 
         return outputdata, dt
-
 
     def merge_files(basefilename, removefiles=False):
 
---
+++ GPRModule/gprMax/__init__.py
@@ -0,0 +1,13 @@
+"""
+======
+gprMax
+======
+
+Electromagnetic wave propagation simulation software.
+
+"""
+
+from ._version import __version__
+from .gprMax import api as run
+
+__name__ = 'gprMax'
