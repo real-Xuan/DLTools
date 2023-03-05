2f8aaa787ec23da88dcb37012fb2f2e3a75c7533
Xuan Zhao <11705197+real-Xuan@users.noreply.github.com>
2023-03-05 16:27:24 +0800

    Update 3DObjectHdf5Generator.py
    Add "random3dArray()" function, which can return [0,1) random 3d array.

--- GPRModule/3DObjectHdf5Generator.py
+++ GPRModule/3DObjectHdf5Generator.py
@@ -2,7 +2,7 @@ import h5py
 import numpy as np
 
 def cone(r=1):
-    a = np.full([64,64,64],-1)
+    a = np.full([64,64,64],1)
     for c in range(64):
         for i in range(64):
             for j in range(64):
@@ -10,6 +10,11 @@ def cone(r=1):
                     a[i][j][c] = 0
         r += 1
     return a
+
+#return [0,1) random 3d array
+def random3dArray():
+    return np.random.random_sample(size=(64, 64, 64))
+
 #write HDF5 file
 f = h5py.File('my_object.h5','w')
 dx_dy_dz = (0.005, 0.005, 0.005)
