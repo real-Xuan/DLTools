91d49bd0cba0b14c56e435af4af4f34782c1ff9d
Xuan Zhao <11705197+real-Xuan@users.noreply.github.com>
2023-03-09 16:01:53 +0800

    Update 3DObjectHdf5Generator.py

--- GPRModule/3DObjectHdf5Generator.py
+++ GPRModule/3DObjectHdf5Generator.py
@@ -1,24 +1,20 @@
 import h5py
 import numpy as np
+import cv2
 
-def cone(r=1):
-    a = np.full([64,64,64],1)
-    for c in range(64):
-        for i in range(64):
-            for j in range(64):
-                if (i - 32) ** 2 + (j - 32) ** 2 < r:
-                    a[i][j][c] = 0
-        r += 1
-    return a
+def imageBinarization():
+    image = cv2.imread('/Users/Xuan/Developer/DLTools/images/root.jpg', 0)
+    cv2.threshold(image, 200, 255, cv2.THRESH_BINARY, image)
+    cv2.imwrite('/Users/Xuan/Developer/DLTools/images/root_binary.jpg', image)
 
 #return [0,1) random 3d array
 def random3dArray():
     return np.random.random_sample(size=(64, 64, 64))
 
 #write HDF5 file
-f = h5py.File('my_object.h5','w')
-dx_dy_dz = (0.005, 0.005, 0.005)
-data = cone()
-data = np.array(data,dtype=np.int16)
-f.attrs['dx_dy_dz'] = dx_dy_dz
-f['/data'] = data
+# f = h5py.File('my_object.h5','w')
+# dx_dy_dz = (0.005, 0.005, 0.005)
+# data = cone()
+# data = np.array(data,dtype=np.float16)
+# f.attrs['dx_dy_dz'] = dx_dy_dz
+# f['/data'] = data
